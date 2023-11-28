beb961ad1a85ac2b5e7e208aa96bfd13a4388863
vidakayla <vidakayla.bensing@edu.sait.ca>
2023-11-28 09:41:21 -0700

    updated js code

--- file.js
+++ file.js
@@ -1,11 +1,8 @@
 //alert 
 document.addEventListener('DOMContentLoaded', function() {
-    // Select the button by its ID
     var alertButton = document.getElementById('alertButton');
 
-    // Add a click event listener to the button
     alertButton.addEventListener('click', function() {
-        // Display an alert when the button is clicked
         alert('Hello world!');
     });
 });
